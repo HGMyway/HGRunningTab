58e9274a2777b9148582937c7aecad75ab37ccc3
HGMyway <616834641@qq.com>
2015-08-18 23:35:50 +0800

    CoreLocation

--- HGRunningTab/HGSUV/Sport/HGSettingSportViewController.swift
+++ HGRunningTab/HGSUV/Sport/HGSettingSportViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import CoreLocation
+
 class HGSettingSportViewController: HGBaseViewController {
     
     
@@ -18,43 +20,58 @@ class HGSettingSportViewController: HGBaseViewController {
     @IBOutlet weak var magnetometerLabel: UILabel!
     
     
-    let hgMotion = HGMotionKit.sharedInstance
+//    let hgMotion = HGMotionKit.sharedInstance
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
         
+        
+        if CLLocationManager.locationServicesEnabled(){
+
+            let  hgLocation = CLLocationManager()
+            hgLocation.delegate = self
+            
+            hgLocation.desiredAccuracy  = kCLLocationAccuracyNearestTenMeters
+//            hgLocation.distanceFilter = 1
+            hgLocation.requestWhenInUseAuthorization()
+//            hgLocation.requestAlwaysAuthorization()
+            hgLocation.startUpdatingLocation()
+
+        }
+        
+        
+        
+        
         //        hgMotion.delegate = self
-        
-        
-        
-        hgMotion.getAccelerometerValues(interval: 1.0) { (x, y, z) -> () in
-            
-            
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                self.acceleLabel.text = "  x = \(x) \n  y = \(y) \n  z = \(z)"
-//                println("x = \(x) y = \(y) z = \(z)")
-            })
-            
-            
-        }
-        
-        hgMotion.getGyroValues(interval: 1.0) { (x, y, z) -> () in
-            
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                self.gyroLabel.text = "  x = \(x) \n  y = \(y) \n  z = \(z)"
-//                println("x = \(x) y = \(y) z = \(z)")
-            })
-        }
-        
-        hgMotion.getMagnetometerValues(interval: 1.0) { (x, y, z) -> () in
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                self.magnetometerLabel.text = "  x = \(x) \n  y = \(y) \n  z = \(z)"
-//                println("x = \(x) y = \(y) z = \(z)")
-            })
-        }
-        
+//        
+//        hgMotion.getAccelerometerValues(interval: 1.0) { (x, y, z) -> () in
+//            
+//            
+//            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                self.acceleLabel.text = "  x = \(x) \n  y = \(y) \n  z = \(z)"
+////                println("x = \(x) y = \(y) z = \(z)")
+//            })
+//            
+//            
+//        }
+//        
+//        hgMotion.getGyroValues(interval: 1.0) { (x, y, z) -> () in
+//            
+//            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                self.gyroLabel.text = "  x = \(x) \n  y = \(y) \n  z = \(z)"
+////                println("x = \(x) y = \(y) z = \(z)")
+//            })
+//        }
+//        
+//        hgMotion.getMagnetometerValues(interval: 1.0) { (x, y, z) -> () in
+//            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                self.magnetometerLabel.text = "  x = \(x) \n  y = \(y) \n  z = \(z)"
+////                println("x = \(x) y = \(y) z = \(z)")
+//            })
+//        }
+//        
         
         
         
@@ -65,9 +82,9 @@ class HGSettingSportViewController: HGBaseViewController {
         super.viewWillDisappear(animated)
         
         
-        hgMotion.stopAccelerometerUpdates()
-        hgMotion.stopGyroUpdates()
-        hgMotion.stopmagnetometerUpdates()
+//        hgMotion.stopAccelerometerUpdates()
+//        hgMotion.stopGyroUpdates()
+//        hgMotion.stopmagnetometerUpdates()
         
         
     }
@@ -104,4 +121,21 @@ class HGSettingSportViewController: HGBaseViewController {
 //}
 
 
+extension HGSettingSportViewController: CLLocationManagerDelegate{
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        
+      let   curLocation = locations.last as! CLLocation
+        
+        println("\(curLocation)")
+        
+        acceleLabel.text = curLocation.description
+        
+        gyroLabel.text = "\(curLocation.coordinate.latitude)  .   \(curLocation.coordinate.longitude)"
+        
+        
+        
+        
+        
+    }
+}
 
